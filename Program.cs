f5264680fda048479c674bb57c169aebdc664bdf
VovaMaybeNextTime <48494728+VovaMaybeNextTime@users.noreply.github.com>
2020-05-27 20:24:30 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -69,7 +69,60 @@ namespace Курсач
             }
             //--------------------------------------------------------------------------------
 
+            //Создание каталога---------------------------------------------------------------
+            Console.Write("Введите необходимый путь по типу C:/Users/Home/Desktop");
+            Console.WriteLine();
+            string name = Console.ReadLine();
+            Console.WriteLine("Ваше место назначения : ");
+            Console.WriteLine(name);
+            Console.Write("Введите название каталога по типу programa");
+            Console.WriteLine();
+            string nameofkat = Console.ReadLine();
+            Console.WriteLine("Ваше название каталога : ");
+            Console.WriteLine(nameofkat);
+            DirectoryInfo dirinfo = new DirectoryInfo(name);
+            if(!dirinfo.Exists)
+            {
+                dirinfo.Create();
+            }
+            dirinfo.CreateSubdirectory(nameofkat);
+            //--------------------------------------------------------------------------------
 
+            //Удаление каталога---------------------------------------------------------------
+            Console.Write("Введите необходимый путь по типу C:/Users/Home/Desktop/programa");
+            Console.WriteLine();
+            string dirName = Console.ReadLine();
+            Console.WriteLine("Ваше место назначения : ");
+            Console.WriteLine(dirName);
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(dirName);
+                dirInfo.Delete(true);
+                Console.WriteLine("Каталог удален");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            //--------------------------------------------------------------------------------
+
+            //Перемещение каталога------------------------------------------------------------
+            Console.Write("Введите название старого каталога по типу C:/Users/Home/Desktop/programa");
+            Console.WriteLine();
+            string oldPath = Console.ReadLine();
+            Console.WriteLine("Введенные данные : ");
+            Console.WriteLine(oldPath);
+            Console.Write("Введите название нового каталога по типу C:/Users/Home/Desktop/programa");
+            Console.WriteLine();
+            string newPath = Console.ReadLine();
+            Console.WriteLine("Введенные данные : ");
+            Console.WriteLine(newPath);
+            DirectoryInfo dirInfo1 = new DirectoryInfo(oldPath);
+            if (dirInfo1.Exists && Directory.Exists(newPath) == false)
+            {
+                dirInfo1.MoveTo(newPath);
+            }
+            //--------------------------------------------------------------------------------
         }
     }
 }
